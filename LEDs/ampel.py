f99a959daf4d2ac4be5b5934a997b23702144a3c
root <root@raspberrypi.(none)>
2013-07-26 11:03:45 +0000

    ampel jpg added

--- LEDs/ampel.py
+++ LEDs/ampel.py
@@ -1,3 +1,25 @@
+########################################################################
+# ampel.py - Trafficlight simulation
+#
+# Copyright (C) 2013 Nico Wollenzin
+#
+# This file is part of Raspi_GPIO_Examples.
+#
+# ampel.py is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# ampel.py is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+#
+# You should have received a copy of the GNU General Public License
+# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+########################################################################
+
 import RPi.GPIO as GPIO
 import time
 
--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Raspi_GPIO_examles
 Some Test Python Scripts for Raspberry PI GPIO
 
 LEDs:
- - ampel.{py|txt} - Traffic lights with switch to change states
+ - ampel.{py|jpg} - Traffic lights with switch to change states
