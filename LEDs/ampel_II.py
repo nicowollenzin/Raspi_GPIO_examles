40a6677c05f07b032274cd232297e0d0d94309d8
root <root@raspberrypi.(none)>
2013-07-27 18:34:42 +0000

    added ampel_II

---
+++ LEDs/ampel_II.py
@@ -0,0 +1,70 @@
+########################################################################
+# ampel.py - Trafficlight simulation
+#
+# Copyright (C) 2013 Nico Wollenzin
+#
+# This file is part of Raspi_GPIO_Examples.
+#
+# ampel.py is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# ampel.py is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+#
+# You should have received a copy of the GNU General Public License
+# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+########################################################################
+
+import RPi.GPIO as GPIO
+import time
+
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup( 3,GPIO.IN)
+GPIO.setup(11,GPIO.OUT)
+GPIO.setup(13,GPIO.OUT)
+GPIO.setup(15,GPIO.OUT)
+status = "gruen" 
+
+def init():
+   #gruene Ampel initalisieren
+   GPIO.output(11,0)
+   GPIO.output(13,0)
+   GPIO.output(15,1)
+
+def set_gruen():
+   time.sleep(0.5)
+   GPIO.output(13,1)
+   time.sleep(1)
+   GPIO.output(11,0)
+   GPIO.output(13,0)
+   GPIO.output(15,1)
+
+def set_rot():
+   time.sleep(0.5)
+   GPIO.output(15,0)
+   GPIO.output(13,1)
+   time.sleep(1)
+   GPIO.output(13,0)
+   GPIO.output(11,1)
+
+try:
+  init()
+  while True:
+     if(GPIO.input(3) == False):
+        if(status == "gruen"):
+           time.sleep(3)
+           print "Ampel Rot"
+           set_rot()
+           status = "rot"
+           time.sleep(10)
+           print "Ampel Gruen"
+           set_gruen()
+           status = "gruen"
+except:      
+  GPIO.cleanup()
--- README.md
+++ README.md
@@ -4,4 +4,5 @@ Raspi_GPIO_examles
 Some Test Python Scripts for Raspberry PI GPIO
 
 LEDs:
- - ampel.{py|jpg} - Traffic lights with switch to change states
+ - ampel.{py|jpg} - Bsic Traffic lights with switch to change states
+ - ampel_II.{py} - Advanced Traffic light with pushbutton logic
